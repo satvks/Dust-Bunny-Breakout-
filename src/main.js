8ca9107d88c3ed8f55fab16354a8359880d74056
Shaurya Bansal <74586008+sbansalucsc@users.noreply.github.com>
2021-05-04 04:33:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+// Group Members: Satvik Srinivasan, Shaurya Bansal, Tate Pieper
+// Game Name: Dust Bunny Breakout
+// Date Completetd: 05/03/2021
+//Creative Tilt: Adding idle/running animations for the bunny. Combining animations with the gameplay was a new and interesting ascpect of our game which we were proud of.
+//In terms of code we implemented physics on objects in unique ways which had an effect on player difficulty. Messing around with the random generation of objects proved to 
+//be a challenge but allowed us to create a fun mechanic for our game.
 // define and configure main Phaser game object
 let config = {
     type: Phaser.CANVAS,
